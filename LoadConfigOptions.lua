b06ea2cec1701178738a51263ae29459920444d1
atomius <atomius1@gmail.com>
2017-01-23 12:45:01 +0100

    started implementing functions LoadConfigOptions() and StripComments() (wip)

--- LoadConfigOptions.lua
+++ LoadConfigOptions.lua
@@ -8,6 +8,41 @@ if DEBUG then STRINGUTIL_ADD_STRING_METHODS = false end
 
 local su = require "stringutil"
 
+
+local function StripComments(line)
+	-- check if the line contains a comment first:
+	--if not line:find("--", 1, true) then return line end
+	--if su.startsWith(line, "--") then return "" end -- shortcut if the whole line is a comment
+	
+	local tmp = ""
+	local minus = string.byte("-")
+	local minusfound = 0
+	
+	for i = 1, #line do
+		local c = line:byte(i)
+		--print(string.char(c))
+		if c == minus then
+			minusfound = minusfound + 1
+			if minusfound == 2 then
+				break
+			end
+			
+		elseif minusfound > 0 then -- there was a minus, but not a second one right after
+			tmp = tmp .. "-" .. string.char(c)
+			minusfound = 0
+			
+		else
+			tmp = tmp .. string.char(c)
+		end
+	end
+	
+	line = tmp
+	
+	print("StripComments: " .. line)
+	return su.trim(line)
+end
+
+
 -- loads all options from file handle 'f' and returns 2 tables:
 -- 1. a table 'options' with all regular options (general and skills)
 -- 2. a table 'tactics', containing a table for each tactic in the same order as they are written
@@ -18,15 +53,29 @@ local su = require "stringutil"
 local function LoadConfigOptions(f)
 	DebugLog("LoadConfigOptions()")
 	
+	local options = {}
+	local tactics = {}
+	
+	for line in f:lines() do
+		--DebugLog(line)
+		line = su.trim(line)
+		
+		repeat -- emulating 'continue' support, use break within this block to 'continue' the loop
+			-- strip comments:
+			line = StripComments(line)
+		until true
+	end
+	
 	-- for each line in f:
 	--     DebugLog(line)
-	--     line = stringutil.strip(line)
+	--     line = stringutil.trim(line)
 	--     repeat
 	--         if 
 	--         if continue_condition then break end
 	--     until true
 	-- end
 	
+	return options, tactics
 end
 
 
