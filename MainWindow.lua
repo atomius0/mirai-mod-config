348cb66798ed610d77d00d32767f3efd4998a2f0
atomius <atomius1@gmail.com>
2017-01-26 16:45:24 +0100

    added test entries to the skill table. (BUG: scrollbar does not appear...)

--- MainWindow.lua
+++ MainWindow.lua
@@ -147,6 +147,7 @@ function MainWindow:init(xmlResource)
 	self:HideLanguageTab(xmlResource) -- TODO: remove this later
 	
 	-- TODO: move this out, into a separate file
+	--[[
 	local skills =
 	{ -- homus:
 		{"Amistr",
@@ -157,6 +158,38 @@ function MainWindow:init(xmlResource)
 		},
 		-- add homus here
 	}
+	--]]
+	local skills =
+	{ -- homus:
+		{"Amistr",
+			{ -- skills:
+				{"Bulwark", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				-- add skills here
+			}
+		},
+		{"Testing a new homu",
+			{
+				{"AAAAAAAAA", 0, "hami_defence.gif", "AS_AMI_BULW"},
+				{"BBBBBBBBB", 1, "hami_defence.gif", "AS_AMI_BULW"},
+				{"CCCCCCCCC", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"DDDDDDDDD", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"EEEEEEEEE", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"AAAAAAAAA", 5, "hami_defence.gif", "AS_AMI_BULW"},
+				{"XXXXXXXXX", 50, "hami_defence.gif", "AS_AMI_BULW"},
+			}
+		}
+		-- add homus here
+	}
+	
 	FillSkillsTab(self.dialog, xmlResource, skills)
 	
 	-- load the configuration file before showing the window:
