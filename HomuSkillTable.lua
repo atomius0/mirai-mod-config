9d49c4cbc10296862bc6712d88d24886664ef8e8
atomius <atomius1@gmail.com>
2017-01-27 14:48:02 +0100

    documented the format of homuSkillTable

--- HomuSkillTable.lua
+++ HomuSkillTable.lua
@@ -1,18 +1,20 @@
+-- HomuSkillTable.lua
+-- a table listing all homunculus skills for the SkillsTab of mirai-mod-conf
+
+-- format:
+-- 'homuSkillTable' is a table containing one table for each Homunculus (homu_table).
+
+-- 'homu_table' looks like this: {homu_name, homu_skills}
+-- where: 'homu_name'   is the name of the homunculus as a string
+--        'homu_skills' is a table that contains one table for each skill (skill_table).
+
+-- 'skill_table' looks like this: {skill_name, max_lvl, icon_file, option_name}
+-- where: 'skill_name'  is the name of the skill
+--        'max_lvl'     is the maximum level the skill can have
+--        'icon_file'   is the icon that will be shown in the GUI, to the left of the skill name
+--        'option_name' is the name of the skill's option table in mirai's "Config.lua".
 
 
---[[ TODO: remove this later
-local homuSkillTable = { -- homus:
-	{"Amistr",
-		{ -- skills:
-			{"Bulwark", 5, "hami_defence.gif", "AS_AMI_BULW"},
-			-- add skills here
-		}
-	},
-	-- add homus here
-}
---]]
-
--- TODO: move this into its own source file later
 local homuSkillTable = { -- homus:
 	{"Amistr",
 		{ -- skills:
