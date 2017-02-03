02c21ad0374d4a49ccba05cbdf36a446b27cf212
atomius <atomius1@gmail.com>
2017-02-03 16:33:00 +0100

    added preliminary dir reading code to ModTab.Init

--- ModTab.lua
+++ ModTab.lua
@@ -1,16 +1,34 @@
 -- mirai-mod-conf
 -- ModTab functions
 
-local su = require "stringutil"
+--local su = require "stringutil" -- do we need this?
 
 local M = {}
+
+
+-- helper function: returns a table containing strings of all filenames in directory dir
+-- whose names end with string 'with'
+local function GetFilesEndingWith(dir, with)
+
+end
 
 
 -- puts all files ending with "_Mod.lua" into the listBox
 function M.Init(listBox)
 	assert(listBox)
+	DebugLog("ModTab.Init")
 	
-	--local dirHandle = wx.wxDir
+	local dirString = wx.wxGetCwd()
+	local dir = wx.wxDir(dirString)
+	assert(dir, string.format("Error opening working directory: %s", dirString))
+	
+	
+	
+	local ok, name = dir:GetFirst("", wx.wxDIR_FILES + wx.wxDIR_HIDDEN)
+	while ok do
+		print(ok, name)
+		ok, name = dir:GetNext(name)
+	end
 	
 	-- TODO: Init
 end
@@ -21,6 +39,7 @@ function M.LoadMod(listBox, fileName)
 	assert(listBox)
 	assert(fileName)
 	-- TODO: LoadMod
+	-- TODO: how to read the selected mod?
 end
 
 
