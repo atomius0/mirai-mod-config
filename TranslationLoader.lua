737020d2755e33bc75f36d9cbb32b4f6bcb4681a
atomius <atomius1@gmail.com>
2017-02-07 13:59:47 +0100

    wrote a simple translation loader module

---
+++ TranslationLoader.lua
@@ -0,0 +1,49 @@
+-- TranslationLoader.lua
+-- simple translation loader
+-- written by atomius, 2017
+
+local M = {}
+
+M.trans = {}
+
+-- loads the translation.
+-- baseLang  is a table containing the strings to be translated
+-- transLang is a table containing the translated strings, ie:
+-- -- baseLang  = {"Hello, World!", "Goodbye"}
+-- -- transLang = {"Hallo, Welt!",  "Auf Wiedersehen"}
+function M.load(baseLang, transLang)
+	assert(type(baseLang ) == "table")
+	assert(type(transLang) == "table")
+	
+	for i = 1, #baseLang do
+		M.trans[baseLang[i]] = transLang[i]
+	end
+end
+
+
+-- the translation function:
+function M._T(s)
+	return M.trans[s] or s
+end
+
+
+local mt = {
+	__call = function(t, ...)
+		return t._T(...)
+	end
+}
+
+return setmetatable(M, mt)
+
+--[[ usage:
+local _T = require "TranslationLoader"
+
+local base  = {"Hello, World!", "Goodbye"}
+local trans = {"Hallo, Welt!",  "Auf Wiedersehen"}
+
+_T.load(base, trans)
+
+print(_T("Hello, World!"))
+print(_T("Goodbye"))
+--]]
+
